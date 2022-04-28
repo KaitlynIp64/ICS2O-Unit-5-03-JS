e758f280aeabdcb3799af675dc469bc1af2d04cb
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-28 17:10:06 +0000

    added code

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Copyright (c) 2022 Kaitlyn Ip All rights reserved
+//
+// Created by: Kaitlyn Ip
+// Created on: Mar 2022
+// This file contains xxx
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-5-02-JS/sw.js", {
+    scope: "/ICS2O-Unit-5-02-JS/",
+  })
+}
+
+/**
+ * This function determines if an integer is negative or positive.
+ */
+function myButtonClicked() {
+  const age = parseInt(document.getElementById("option-1").value)
+  const age = parseInt(document.getElementById("option-2").value)
+  const age = parseInt(document.getElementById("option-3").value)
+  const age = parseInt(document.getElementById("option-4").value)
+// if ... then ... elseif ... else example
+  if (age == "option-1") {
+    console.log("You can watch R-rated movies alone.")
+  } 
+  else if (age == "option-2") {
+    console.log("You can watch pg-13 rated movies alone.")
+  } 
+  else if (age == "option-3") {
+    console.log("You can watch G-rated movies alone.")
+  } 
+  else {
+    console.log("You are too young to watch movies alone.") 
+  }
+}
